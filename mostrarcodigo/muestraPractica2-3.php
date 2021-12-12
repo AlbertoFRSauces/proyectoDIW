08d0809ba372eb77681c31e00dbda330b1d7f45a
Alberto <alberto.ferram@educa.jcyl.es>
2021-12-12 01:49:27 +0100

    12/12/2021 Fix README Modificacion del Index Modificacion del CSS Add .htaccess Add archivos de errores 400, 403, 404, 500 Add archivos de muestra del codigo de las practicas Add carpetas de las practicas con los archivos

---
+++ mostrarcodigo/muestraPractica2-3.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="author" content="Alberto Fernandez Ramirez">
+        <title>Practica 2-3</title>
+    </head>
+    <body>
+    <?php
+        highlight_file("../doc/Practica2-3/index.html");
+    ?>
+    </body>
+</html>
+
---
+++ mostrarcodigo/muestraPractica2-9.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="author" content="Alberto Fernandez Ramirez">
+        <title>Practica 2-9</title>
+    </head>
+    <body>
+    <?php
+        highlight_file("../doc/Practica2-9/index.html");
+    ?>
+    </body>
+</html>
+
